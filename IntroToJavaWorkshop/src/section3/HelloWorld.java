2e671d8da8b8b54cf65a0bb4856256c335a9553c
Emilio47 <37815818+Emilio47@users.noreply.github.com>
2018-03-26 14:01:18 -0700

    Add files via upload

---
+++ IntroToJavaWorkshop/src/section3/HelloWorld.java
@@ -0,0 +1,11 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello world");
+		String name = JOptionPane.showInputDialog("Enter your name");
+		JOptionPane.showMessageDialog(null, "Nice to meet you "+name);
+	}
+}
